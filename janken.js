51cb077c22d1c83a40397488d104e4a714ef4be7
samarjeet2004 <samarjeetsingh1969m@gmail.com>
2026-05-02 15:48:30 +0000

    Implement interactive UI and refactor game flow
    1.Refactor playGame to use DOM manipulation for game flow.
    2.Remove alert() and prompt() in favor of event-driven logic.
    3.Add buttons and result containers to index.html.
    4.Fix logic errors regarding element appending to the document body.

--- janken.js
+++ janken.js
@@ -14,52 +14,124 @@ function getComputerChoice(){
 }
 
 
-function getHumanChoice(){
-  const choice = prompt("Enter your choice: ");
-  return choice;
-}
+
 
 
 function playGame(){
   let humanScore  =0;
   let computerScore = 0;
+  function showGmeOver(){
+    
+      const gameOver = document.createElement("div");
+      gameOver.textContent = "Game has concluded! Please try again.";
+      document.body.appendChild(gameOver);
+    
+  }
+  
+  function updateRoundResult(txt){
+    const roundResult = document.createElement("div");
+    roundResult.textContent = txt;
+    const score = document.createElement("span");
+    score.textContent = `Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    document.body.appendChild(roundResult);
+    document.body.appendChild(score);
+  }
   let playRound = (humanChoice,computerChoice)=>{
     humanChoice = humanChoice.toLowerCase();
+   
     if ((humanChoice==="rock")&&(computerChoice==="paper")){
       computerScore+=1;
-      console.log("You lose! Paper beats Rock ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You lose! Paper beats Rock `);
     }else if  ((humanChoice==="paper")&&(computerChoice==="scissors")){
       computerScore+=1;
-      console.log("You lose! Scissors beats Paper ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You lose! Scissors beats Paper `);
     }else if ((humanChoice==="rock")&&(computerChoice==="scissors")){
       humanScore+=1;
-      console.log("You win! Rock beats Scissors ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You win! Rock beats Scissors `);
     }else if  ((humanChoice==="scissors")&&(computerChoice==="rock")){
       computerScore+=1;
-      console.log("You lose! Rock beats Scissors ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You lose! Rock beats Scissors `);
     }else if ((humanChoice==="scissors")&&(computerChoice==="paper")){
       humanScore+=1;
-      console.log("You win! Scissors beats Paper ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You win! Scissors beats Paper `);
     }else if ((humanChoice==="paper")&&(computerChoice==="rock")){
       humanScore+=1;
-      console.log("You win! Paper beats Rock ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. You win! Paper beats Rock `);
     }else if (humanChoice===computerChoice){
-      console.log("Tie! ");
+      updateRoundResult(`Your Choice: ${humanChoice} Computer Choice: ${computerChoice}. Tie! `);
     }
+
+    showMatchWinner();
   }
-  for (let i = 0 ; i<5;i++){
+  /*for (let i = 0 ; i<5;i++){
     humanSelection = getHumanChoice();
     computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+  }*/
+
+  const rock = document.querySelector(".rock");
+  const paper = document.querySelector(".paper");
+  const scissors = document.querySelector(".scissors");
+
+  let computerSelection =  getComputerChoice();
+  console.log(rock);
+  
+
+  rock.addEventListener("click",(e)=>{
+    
+    if ((humanScore===5)||(computerScore===5)){
+      showGmeOver();
+      return;
+    }
+    let humanSelection  = "rock";
+    playRound(humanSelection,computerSelection);
+    
+   
+  });
+
+  paper.addEventListener("click",(e)=>{
+    
+    if ((humanScore===5)||(computerScore===5)){
+      showGmeOver();
+      return;
+    }
+    let humanSelection  = "paper";
+    playRound(humanSelection,computerSelection);
+    
+    
+  });
+
+  scissors.addEventListener("click",(e)=>{
+    
+    if ((humanScore===5)||(computerScore===5)){
+      showGmeOver();
+      return;
+    }
+    let humanSelection  = "scissors";
+    playRound(humanSelection,computerSelection);
+    
+    
+  });
+
+  function showMatchWinner(){
+    const finalResult = document.createElement("div");
+    if ((humanScore===5)||(computerScore===5)){
+      if (humanScore===5){
+        finalResult.textContent = "You won the match!";
+      }else if(computerScore===5){
+        finalResult.textContent = "You lost the match!"
+      }
+      document.body.appendChild(finalResult);
+    }
   }
 
-  if (computerScore>humanScore){
-    console.log("You lose! ");
+  /*if (computerScore>humanScore){
+    updateRoundResult("You lose! ");
   }else if (computerScore<humanScore){
-    console.log("You win! ");
+    updateRoundResult("You win! ");
   }else{
-    console.log("Tie!");
-  }
+    updateRoundResult("Tie!");
+  }*/
 }
 
 playGame();
